2f1801561ea988c05aa594e84c6e9b7567a53e99
cast-soliveira <samuel.oliveira@castgroup.com.br>
2023-10-21 18:27:24 -0300

    initial commit release

---
+++ frontend-antares/src/layouts/LoginLayout.vue
@@ -0,0 +1,201 @@
+<template>
+  <div id="login" class="column items-center justify-center">
+    <div class="background-image"></div>
+    <q-card class="my-card">
+      <q-card-section class="bg-blue text-white">
+        <div class="text-h4 text-center">UnB na Mão</div>
+        <div class="text-h6 text-center low-opacity">{{ registrando ? 'Registre-se' : 'Fazer login' }}</div>
+        <div>
+          <q-input filled v-if="registrando" v-model="email" dense class="bg-white q-mt-md" label="E-mail *"/>
+          <q-input filled v-model="usuario" dense class="bg-white q-my-md" label="Usuário *"/>
+          <q-select filled v-if="registrando" v-model="role" dense class="bg-white" :options="roleOptions" label="Tipo de Conta *"></q-select>
+          <q-input filled v-model="senha" dense class="bg-white q-mt-md" label="Senha *" :type="isPwd ? 'password' : 'text'">
+            <template v-slot:append>
+              <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer q-pr-sm"
+                @click="isPwd = !isPwd"
+              />
+            </template>
+          </q-input>
+          <div v-if="registrando">
+            <q-input filled v-model="confirmarSenha" dense class="bg-white q-mt-md" label="Confirmar Senha *" :type="isConfirmePwd ? 'password' : 'text'">
+              <template v-slot:append>
+                <q-icon
+                  :name="isPwd ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer q-pr-sm"
+                  @click="isConfirmePwd = !isConfirmePwd"
+                />
+              </template>
+            </q-input>
+          </div>
+        </div>
+      </q-card-section>
+      <div v-if="!registrando">
+        <q-card-actions class="bg-green row justify-around">
+          <q-btn flat class="text-white" @click="logar">Entrar</q-btn>
+        </q-card-actions>
+        <q-card-actions class="bg-blue row justify-around">
+          <q-btn id="registrar" @click="telaRegistrar" flat class="text-white">Registrar-se</q-btn
+          >
+        </q-card-actions>
+      </div>
+      <div v-if="registrando">
+        <q-card-actions class="bg-green row justify-around">
+          <q-btn filled @click="registrar" flat class="text-white">Finalizar Cadastro</q-btn>
+        </q-card-actions>
+        <q-card-actions class="bg-blue row justify-around">
+          <q-btn id="registrar" @click="cancelarRegistro" flat class="text-white">Cancelar</q-btn>
+        </q-card-actions>
+      </div>
+    </q-card>
+    <button filled v-if="!registrando" id="button" flat class="q-mt-sm bg-yellow-10 text-white q-pa-sm">
+      Esqueci minha Senha
+    </button>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const confirmarSenha = ref(null);
+const registrando = ref(false);
+const isPwd = ref(true);
+const isConfirmePwd = ref(true);
+const usuario  = ref(null);
+const senha = ref(null);
+const email = ref(null)
+
+const role = ref(null);
+const roleOptions = ref(['Estudante', 'Atlética', 'Empresa Júnior']);
+
+function logar() {
+  router.push('/home');
+}
+
+function telaRegistrar() {
+  registrando.value = true;
+}
+
+function cancelarRegistro() {
+  registrando.value = false;
+}
+
+const createRegistrarObject = () => {
+  let registroValido = false
+  const registroObject = {
+    email: email.value,
+    usuario: usuario.value,
+    senha: senha.value,
+    role: role.value
+  }
+
+  registroValido = validarRegistro(registroObject)
+  if(registroValido) {
+    return registroObject
+  } else {
+    return false
+  }
+}
+
+function validarRegistro(registroObject: any) {
+  // Colocar Validações do Obejto Registrar Aqui
+  if (registroObject.email == null || registroObject.usuario == null || registroObject.senha == null || registroObject.role == null) {
+    alert('Preencha todos os campos')
+    return false
+  }
+
+  return true
+}
+
+function registrar () {
+  const requestRegistro = createRegistrarObject()
+  if(requestRegistro == false) {
+    return
+  }
+  alert('Cadastrou com Sucesso\n' + JSON.stringify(requestRegistro))
+  registrando.value = false
+}
+
+</script>
+<style scoped>
+* {
+  overflow: hidden !important;
+}
+
+#login {
+  width: 100vw;
+  height: 100vh;
+  /* Remova o background-image do #login */
+  position: relative; /* Adicione position: relative; */
+}
+
+.background-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('assets/imgs/login-bg.jpg');
+  background-size: cover;
+  animation: zoom 60s linear infinite;
+}
+
+@keyframes zoom {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+.q-btn {
+  width: 100%;
+  height: 100%;
+}
+
+#button {
+  position: absolute;
+  bottom: 40px;
+  outline: none;
+  border: none;
+  opacity: 0.95;
+  transition: all 0.2s;
+  cursor: pointer;
+  z-index: 0;
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
+}
+
+#registrar {
+  font-size: 0.8rem;
+  opacity: 0.6;
+  transition: all 0.2s;
+}
+
+q-btn:hover,
+#button:hover {
+  opacity: 1;
+}
+
+.q-card {
+  width: 25rem;
+}
+
+@media (max-width: 425px) {
+  .q-card {
+    width: 90vw;
+  }
+}
+
+@media (max-width: 800px) {
+  .background-image {
+    animation: none !important;
+  }
+}
+</style>
---
+++ frontend-antares/src/router/routes.ts
@@ -0,0 +1,24 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    name: 'login',
+    path: '/',
+    component: () => import('layouts/LoginLayout.vue'),
+  },
+  {
+    name: 'home',
+    path: '/home',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+];
+
+export default routes;
