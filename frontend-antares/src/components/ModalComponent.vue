ff958b22323dad550c0cb242b4f056c6ec1b39c1
Matheus Ferreira <200024949@aluno.unb.br>
2023-11-22 23:37:28 -0300

    inserindo os dados na modal

---
+++ frontend-antares/src/components/ModalComponent.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="modal">
+        <div class="modal-content">
+            <div class="modal-header">
+                <slot class="modal-title"></slot>
+            </div>
+            <div class="modal-body">
+                <slot name="bodyModal"></slot>
+            </div>
+            <div class="modal-input">
+                <slot name="inputModal1"></slot>
+            </div>
+            <div class="modal-input">
+                <slot name="inputModal2"></slot>
+            </div>
+            <div class="modal-footer">
+                <slot name="botaoConfirmar"></slot>
+                <slot name="botaoFechar"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ModalComponent',
+    props: {
+        placeholderText: String,
+        buttons: Array,
+    },
+
+    methods: {
+        fecharModal() {
+            this.$emit('fecharModal')
+        }
+    }
+};
+</script>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.modal {
+    position: fixed;
+    transform: translate(-50%, -50%);
+    top: 50%;
+    left: 50%;
+    background: rgba(0, 0, 0, 0.4);
+    width: flex;
+    height: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    overflow-y: auto;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+
+.modal-content {
+    flex-grow: 1;
+    flex-direction: column;
+    display: flex;
+    width: auto;
+    margin: auto;
+    min-width: none;
+    background: #fff;
+    margin: auto;
+    padding-top: 120px;
+}
+
+.modal-header {
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+.modal-input {
+    padding: 5px;
+}
+
+.modal-title {
+    font-weight: bold;
+    font-size: 12px;
+    text-align: center;
+}
+
+.modal-close {
+    padding: 5px;
+    cursor: pointer;
+}
+
+.modal-body {
+    flex-grow: 1;
+    height: 100%;
+    padding: 10px;
+    text-align: justify;
+}</style>
--- frontend-antares/src/layouts/FeedLayout.vue
+++ frontend-antares/src/layouts/FeedLayout.vue
@@ -1,89 +1,100 @@
 <template>
     <q-layout view="hHh lpR lfr">
-   <q-header bordered class="bg-blue text-white">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar class="q-mr-sm">
-            <img src="https://pbs.twimg.com/profile_images/1696145651006930945/r5LfokUU_400x400.jpg">
-          </q-avatar>
-          UnB na Mão
-        </q-toolbar-title>
+        <q-header bordered class="bg-blue text-white">
+            <q-toolbar>
+                <q-toolbar-title>
+                    <q-avatar class="q-mr-sm">
+                        <img src="https://pbs.twimg.com/profile_images/1696145651006930945/r5LfokUU_400x400.jpg">
+                    </q-avatar>
+                    UnB na Mão
+                </q-toolbar-title>
 
-        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
-      </q-toolbar>
-    </q-header>
+                <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+            </q-toolbar>
+        </q-header>
 
-    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
-      <q-img class="absolute-top" style="background: rgb(36, 36, 36); height: 200px">
-        <div class="absolute-center column items-center bg-transparent">
-          <q-avatar size="80px" class="q-mb-sm">
-            <img :src="sessionData.user_image">
-          </q-avatar>
-          <div class="text-weight-bold">{{ sessionData.nome }}</div>
-          <div>@{{ sessionData.login }}</div>
-        </div>
-      </q-img>
-    <!-- items do menu -->
-    <q-scroll-area style="height: calc(100% - 200px); margin-top: 200px; border-right: 1px solid #ddd">
-      <q-list padding>
-        <q-item active clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon name="star" />
-          </q-item-section>
+        <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+            <q-img class="absolute-top" style="background: rgb(36, 36, 36); height: 200px">
+                <div class="absolute-center column items-center bg-transparent">
+                    <q-avatar size="80px" class="q-mb-sm">
+                        <img :src="sessionData.user_image">
+                    </q-avatar>
+                    <div class="text-weight-bold">{{ sessionData.nome }}</div>
+                    <div>@{{ sessionData.login }}</div>
+                </div>
+            </q-img>
+            <!-- items do menu -->
+            <q-scroll-area style="height: calc(100% - 200px); margin-top: 200px; border-right: 1px solid #ddd">
+                <q-list padding>
+                    <q-item active clickable v-ripple>
+                        <q-item-section avatar>
+                            <q-icon name="star" />
+                        </q-item-section>
 
-          <q-item-section>
-            Star
-          </q-item-section>
-        </q-item>
+                        <q-item-section>
+                            Star
+                        </q-item-section>
+                    </q-item>
 
-        <q-item clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon name="send" />
-          </q-item-section>
+                    <q-item clickable v-ripple>
+                        <q-item-section avatar>
+                            <q-icon name="send" />
+                        </q-item-section>
 
-          <q-item-section>
-            Send
-          </q-item-section>
-        </q-item>
+                        <q-item-section>
+                            Send
+                        </q-item-section>
+                    </q-item>
 
-        <q-item clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon name="drafts" />
-          </q-item-section>
+                    <q-item clickable v-ripple>
+                        <q-item-section avatar>
+                            <q-icon name="drafts" />
+                        </q-item-section>
 
-          <q-item-section>
-            Drafts
-          </q-item-section>
-        </q-item>
-        
-        <q-item clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon name="settings" />
-          </q-item-section>
+                        <q-item-section>
+                            Drafts
+                        </q-item-section>
+                    </q-item>
 
-          <q-item-section>
-            Configurações
-          </q-item-section>
-        </q-item>
-        <q-item clickable v-ripple @click="logout()">
-          <q-item-section avatar>
-            <q-icon name="logout" />
-          </q-item-section>
+                    <q-item clickable v-ripple>
+                        <q-item-section avatar>
+                            <q-icon name="settings" />
+                        </q-item-section>
 
-          <q-item-section>
-            Fazer Logout
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-scroll-area>
-    </q-drawer>
-    <h1 class="title-page">Feed de notícias</h1>
-    <q-card class="card-feed" v-for="(news, index) in vectorNews" :key="index"> 
-    <q-card-section>
-        <h2 class="title-card">{{ news.title }}</h2>
-        <div>{{ news.body }}</div>
-    </q-card-section>
-    </q-card>
+                        <q-item-section>
+                            Configurações
+                        </q-item-section>
+                    </q-item>
+                    <q-item clickable v-ripple @click="logout()">
+                        <q-item-section avatar>
+                            <q-icon name="logout" />
+                        </q-item-section>
+
+                        <q-item-section>
+                            Fazer Logout
+                        </q-item-section>
+                    </q-item>
+                </q-list>
+            </q-scroll-area>
+        </q-drawer>
+        <h1 class="title-page">Feed de notícias</h1>
+        <q-card class="card-feed" v-for="(news, index) in vectorNews" :key="index" @click="openModalNews(news)">
+            <q-card-section>
+                <h2 class="title-card">{{ news.title }}</h2>
+                <div>{{ news.body }}</div>
+            </q-card-section>
+        </q-card>
+        <ModalComponent v-if = "modalNewsOpen" @fecharModal="fecharModalNews" class="modal-component">
+            <slot name="modal-title" class="modal-title">
+                <h2>{{ selectedNews.title }}</h2>
+            </slot>
+            <slot name="modal-body">
+                <h2>{{ selectedNews.body }}</h2>
+            </slot>
+            <template v-slot:botaoFechar>
+                <q-btn label="Fechar" color="primary" @click="fecharModalNews" />
+            </template>
+        </ModalComponent>
     </q-layout>
 </template>
 
@@ -92,40 +103,63 @@ import { computed, ref } from 'vue'
 import { useSessionStore } from 'src/stores/session';
 import { useRouter } from 'vue-router';
 import { data } from 'autoprefixer';
+import ModalComponent from '../components/ModalComponent.vue'
 
-const vectorNews=[{ title: 'Título da Notícia 1', body: 'Corpo da Notícia 1' },
-        { title: 'Título da Notícia 2', body: 'Corpo da Notícia 2'  }]
+const vectorNews = [{ title: 'Título da Notícia 1', body: 'Corpo da Notícia 1' },
+{ title: 'Título da Notícia 2', body: 'Corpo da Notícia 2' }]
+const modalNewsOpen = ref(false)
+const selectedNews = ref({ title: '', body: '' })
 
 const router = useRouter()
 
 const rightDrawerOpen = ref(true)
-function toggleRightDrawer () {
-  rightDrawerOpen.value = !rightDrawerOpen.value
+function toggleRightDrawer() {
+    rightDrawerOpen.value = !rightDrawerOpen.value
 }
 const sessionStore = useSessionStore();
 const sessionData = computed(() => sessionStore.getSessionData) as any
 
-function logout () {
-  const res = confirm('Deseja Fazer Logout ?')
-  if(res) {
-    sessionStore.clearSessionData()
-    window.location.href = 'https://unbnamao.netlify.app'
-    
-  }
+function logout() {
+    const res = confirm('Deseja Fazer Logout ?')
+    if (res) {
+        sessionStore.clearSessionData()
+        window.location.href = 'https://unbnamao.netlify.app'
+
+    }
+}
+
+function openModalNews(news: any) {
+    selectedNews.value = { title: news.title, body: news.body}
+    modalNewsOpen.value = true
+}
+
+function fecharModalNews() {
+    modalNewsOpen.value = false
 }
 </script>
 <style>
-
-.title-page{
+.title-page {
     font-size: 30px;
     margin-top: 30px;
     margin-left: 20px;
 }
-.card-feed{
+
+.card-feed {
     margin-left: 100px;
     margin-right: 100px;
 }
-.title-card{
+
+.title-card {
     font-size: 20px;
+}
+.modal-component {
+    width: 50%;
+    height: 50%;
+    margin: auto;
+}
+.modal-title {
+    font-size: 20px;
+    font-weight: bold;
+    text-align: center;
 }
 </style>
