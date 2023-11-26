76674b506b86d9563b1160fe289cdeb936f86caf
LucasGSAntunes <190091681@aluno.unb.br>
2023-11-26 18:54:04 -0300

    Funcao busca todas as vagas do feed

--- backend-antares/src/routes/router.js
+++ backend-antares/src/routes/router.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const usuariosRouter = require('./usuarios');
 const vagasRouter = require('./vagas'); 
+const feedRouter = require('./feed')
 
 router.use('/', usuariosRouter);
 router.use('/', vagasRouter);
+router.use('/', feedRouter);
 
 module.exports = router;
