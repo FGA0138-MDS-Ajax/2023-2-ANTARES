5d2f52e434147256e05d40de319fbc79c233be81
Samuel Victor Oliveira Lima <990905>
2023-11-27 15:49:37 -0300

    integra paginação feed

--- frontend-antares/src/composables/UseFeedApi.ts
+++ frontend-antares/src/composables/UseFeedApi.ts
@@ -1,10 +1,10 @@
 import { api } from 'src/boot/axios';
 
-export default function useFeedApi(_params: any) {
+export default function useFeedApi(params: any) {
 
   const listarFeed = async () => {
     try {
-      const response = await api.get('/feed');
+      const response = await api.post('/feed', params);
       return response;
     } catch (error) {
       throw error;
--- frontend-antares/src/pages/FeedPage.vue
+++ frontend-antares/src/pages/FeedPage.vue
@@ -1,17 +1,25 @@
 <template>
     <q-page>
-        <h2 class="text-center q-py-lg">Feed de publicações</h2>
+        <h2 class="w100 text-center q-py-lg">Feed de publicações</h2>
         <div class="column q-px-lg q-gutter-y-md">
             <div class="q-pt-xl" v-if="vectorNews.length == 0">
                 <h5 class="text-center">Não há publicações no momento</h5>
                 <p style="font-size:100px" class="text-center q-pt-md">&#128531;</p>
             </div>
-            <q-card class="cursor-pointer" v-for="(news, index) in vectorNews" :key="index" @click="openModalNews(news)">
-            <q-card-section>
-                    <h4>{{ news.titulo }}</h4>
-                    <div class="q-pt-sm">{{ news.descricao}}</div>
-                </q-card-section>
-            </q-card>
+            <div class="cards row justify-center q-gutter-md q-mt-md">
+                <q-card class="cursor-pointer" v-for="(news, index) in vectorNews" :key="index" @click="openModalNews(news)">
+                    <q-card-section style="height: 8rem">
+                            <h4>{{ news.titulo }}</h4>
+                            <div class="q-pt-sm">{{ news.criadorEmail}}</div>
+                            <h8 class="low-opacity">Postado em: {{ news.dataPublicacao.toLocaleDateString('pt-br') }}</h8>
+                        </q-card-section>
+                    </q-card>
+            </div>
+            <div v-if="vectorNews.length != 0" class="row w100 justify-center items-center q-py-md">
+                <q-icon @click="atualizarPaginacao(-1)" class="cursor-pointer text-bold q-pr-md" size="md" :color="paginaAtual == 0 ? 'grey-5' : 'blue-7'" name="skip_previous"/>
+                <div class="">ver mais vagas</div>
+                <q-icon @click="atualizarPaginacao(1)" class="cursor-pointer text-bold q-pl-md" size="md" :color="paginaAtual + 1 == totalPaginas ? 'grey-5' : 'blue-7'" name="skip_next"/>
+            </div>
         </div>
         <div class="modal-wrap" v-if="modalNewsOpen">
             <ModalComponent @fecharModal="fecharModalNews" class="modal-component rounded-borders">
@@ -52,7 +60,8 @@ import ModalComponent from '../components/ModalComponentFeed.vue'
 import FeedService from '../services/FeedService'
 
 const vectorNews = ref([]) as any
-
+const paginaAtual = ref(0)
+const totalPaginas = ref(0)
 
 const modalNewsOpen = ref(false)
 const selectedNews = ref(null) as any
@@ -71,11 +80,13 @@ function openLink(link: string) {
     window.open(link, '_blank')
 }
 
-onBeforeMount(async () => {
+async function atualizarPaginacao (num: number) {
+    paginaAtual.value += num
     try {
-        const { listarFeed } = FeedService(null);
+        const { listarFeed } = FeedService({numPagina: paginaAtual.value});
         const response = await listarFeed();
-        console.log(JSON.stringify(response.data.vagas))
+        console.log(JSON.stringify(response.data.totalPages))
+        totalPaginas.value = response.data.totalPages
         vectorNews.value = response.data.vagas
         vectorNews.value.forEach((element: any) => {
             element.dataPublicacao = new Date(element.dataPublicacao)
@@ -85,16 +96,17 @@ onBeforeMount(async () => {
     catch (error) {
         console.log(error)
     }
+}
+
+onBeforeMount(async () => {
+    await atualizarPaginacao(0)
 })
 </script>
 
-<style>
+<style scoped>
 
 .q-page {
-    background: #fafafacf;  /* fallback for old browsers */
-    background: -webkit-linear-gradient(to bottom, #fafafacf, #c9d6ff83);  /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(to bottom, #fafafacf, #c9d6ff83); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
+    background-color: #c8c8c8;
 }
 
 .modal-wrap{
@@ -146,6 +158,19 @@ onBeforeMount(async () => {
     height: 400px;
 }
 
+.q-card{
+    background: #4fb151;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to bottom, #4fb151, #107e44);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to bottom, #4fb151, #107e44); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    color: #fafafa;
+    width: 25%;
+}
+
+.q-card:hover {
+    transition: all 0.2s linear;
+    opacity: .8;
+}
+
 @media (max-width: 1200px) {
     .modal-component{
         width: 90%;
@@ -161,6 +186,13 @@ onBeforeMount(async () => {
     .q-scroll-area{
         height: 200px!important;
     }
+    .q-card {
+        width: 95%!important;
+    }
+    .cards {
+        display: flex;
+        flex-direction: column;
+    }   
 }
 
 p{
