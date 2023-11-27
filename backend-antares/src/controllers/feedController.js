36cf92b2641d0a384e29cd8772c81b198b0d3da0
Samuel Victor Oliveira Lima <990905>
2023-11-27 10:47:10 -0300

    cria paginacao no backend

--- backend-antares/src/controllers/feedController.js
+++ backend-antares/src/controllers/feedController.js
@@ -3,15 +3,30 @@ const VagaModel = require('../models/Vagas');
 const feedController = {
     getAll: async (req, res) => {
         try {
-            const vagas = await VagaModel.find();
+            const numPagina = req.body.numPagina || 0;
+            const porPagina = 15;
 
-            res.status(201).json({vagas});
-        }
-        catch (error) {
+            // Consulta para obter as vagas na página atual
+            const vagas = await VagaModel.find()
+                .sort({ dataPublicacao: -1 })
+                .skip(numPagina * porPagina)
+                .limit(porPagina);
+
+            // Consulta para obter o número total de vagas
+            const totalVagas = await VagaModel.countDocuments();
+
+            // Calcula o número total de páginas
+            const totalPages = Math.ceil(totalVagas / porPagina);
+
+            // Retorna o resultado com totalPages e totalVagas
+            res.status(200).json({ vagas, totalPages, totalVagas });
+        } catch (error) {
             console.error(error);
-            res.status(500).json({ error: 'Erro ao obter vagas'});
+            res.status(500).json({ error: 'Erro ao obter vagas' });
         }
     },
 };
+
+
 
 module.exports = feedController;
