94661e511d6730cfa1de1935cbaf172d49513b1a
matix0 <mateusviniciuspw@gmail.com>
2023-12-04 03:07:24 -0300

    metodo delete adicionado

--- backend-antares/src/controllers/calendarioController.js
+++ backend-antares/src/controllers/calendarioController.js
@@ -1,84 +1,103 @@
-const { Calendario: CalendarioModel} = require('../models/Calendario');
+const { Calendario: CalendarioModel } = require("../models/Calendario");
+const { findByIdAndDelete, deleteOne } = require("../models/Vagas");
 
 const calendarioController = {
-    create: async (req, res) => {
-        const eventos = CalendarioModel.find(req.body.login);
-        try {
-            const verificaLogin = await CalendarioModel.findOne({ login: req.body.login });
-            console.log(verificaLogin);
-            if (verificaLogin) {
-                const eventos = await CalendarioModel.findOne({ login: req.body.login }).select('eventos');
-                const newEvento = {
-                    titulo: req.body.titulo,
-                    dataEvento: req.body.dataEvento
-                }
-                eventos.eventos.push(newEvento);
+  create: async (req, res) => {
+    const eventos = CalendarioModel.find(req.body.login);
+    try {
+      const verificaLogin = await CalendarioModel.findOne({
+        login: req.body.login,
+      });
+      console.log(verificaLogin);
+      if (verificaLogin) {
+        const eventos = await CalendarioModel.findOne({
+          login: req.body.login,
+        }).select("eventos");
+        const newEvento = {
+          titulo: req.body.titulo,
+          dataEvento: Date.now(),
+        };
+        eventos.eventos.push(newEvento);
 
-                const response = await CalendarioModel.findOneAndUpdate({ login: req.body.login }, eventos, { new: true });
-                res.status(201).json({ response, message: 'Evento criado com sucesso.' });
+        const response = await CalendarioModel.findOneAndUpdate(
+          { login: req.body.login },
+          eventos,
+          { new: true }
+        );
+        res
+          .status(201)
+          .json({ response, message: "Evento criado com sucesso." });
+      } else {
+        const newEvento = {
+          login: req.body.login,
+          eventos: { titulo: req.body.titulo, dataEvento: Date.now() },
+        };
 
-            }
-            else {
-                const newEvento = {
-                    login: req.body.login,
-                    eventos: { titulo: req.body.titulo, dataEvento: Date.now() }
-                };
+        const response = await CalendarioModel.create(newEvento);
+        res
+          .status(201)
+          .json({ response, message: "Evento criado com sucesso." });
+      }
+    } catch (error) {
+      console.error("Erro controller calendario\n" + error);
+    }
+  },
 
-                const response = await CalendarioModel.create(newEvento);
-                res.status(201).json({ response, message: 'Evento criado com sucesso.' });
-            }
+  getAll: async (req, res) => {
+    try {
+      console.log(req.body.login);
+      const calendario = await CalendarioModel.findOne({
+        login: req.body.login,
+      });
+      res.status(200).json({ calendario });
+    } catch (error) {
+      console.error("Eventos não encontrados \n" + error);
+      res.status(500).json({ error: "Erro ao obter eventos" });
+    }
+  },
 
-        } catch (error) {
-            console.error('Erro controller calendario\n' + error);
+  getDay: async (req, res) => {
+    try {
+      const calendario = await CalendarioModel.findOne({
+        login: req.body.login,
+      });
+      console.log(calendario);
+      const eventosDia = calendario.eventos.filter(
+        (evento) => evento.dataEvento == req.body.dataEvento
+      );
+      console.log(eventosDia);
+      console.log(req.body.dataEvento);
+
+      res.status(200).json({ calendario });
+    } catch (error) {
+      console.error("Eventos não encontrados \n" + error);
+      res.status(500).json({ error: "Erro ao obter eventos" });
+    }
+  },
+
+  delete: async (req, res) => {
+    try {
+      const response = await CalendarioModel.updateOne(
+        { login: req.body.login },
+        {
+          $pull: {
+            eventos: { titulo: req.body.titulo }
+          }
         }
-    },
-
-    getAll: async (req, res) => {
-        try {
-            console.log(req.body.login);
-            const calendario = await CalendarioModel.findOne({login: req.body.login});
-            res.status(200).json({ calendario });
-        } catch (error) {
-            console.error('Eventos não encontrados \n' + error);
-            res.status(500).json({ error: 'Erro ao obter eventos' });
-        }
-    },
-
-    getDay: async (req, res) => {
-        try {
-            const calendario = await CalendarioModel.findOne({login: req.body.login});
-            console.log(calendario);
-            const eventosDia = calendario.eventos.filter(evento => evento.dataEvento == req.body.dataEvento);
-            console.log(eventosDia);
-            console.log(req.body.dataEvento);
-
-            res.status(200).json({ calendario });
-        } catch (error) {
-            console.error('Eventos não encontrados \n' + error);
-            res.status(500).json({ error: 'Erro ao obter eventos' });
-        }
-    },
-
-
-    removerEvento: async (req, res) => {
-        try {
-            const eventos = await CalendarioModel.findOne({ login: req.body.login });
-            const evento = eventos.eventos.filter(evento => evento.titulo == req.body.titulo);
-
-            const response = await CalendarioModel.deleteMany({ 'eventos.titulo': req.body.titulo }).where('eventos.titulo').equals(req.body.titulo);        
-                               
-            //const response = await CalendarioModel.deleteMany({ login: req.body.login }, evento);
-
-            res.status(200).json({ response, message: 'Evento removido com sucesso.' });
-
-        } catch (error) {
-            console.error('Erro ao remover evento \n' + error);
-            res.status(500).json({ error: 'Erro ao remover evento' });
-        }
-    },
-
+      );
+  
+      if (response.modifiedCount === 0) {
+        res.status(404).json({ message: "Evento não encontrado." });
+        return;
+      }
+  
+      res.status(200).json({ response, message: "Evento deletado." });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Erro ao deletar o Evento.", error: error.message });
+    }
+  },
+  
 };
-
-
-
 module.exports = calendarioController;
--- backend-antares/src/routes/calendario.js
+++ backend-antares/src/routes/calendario.js
@@ -4,9 +4,10 @@ const calendarioController = require('../controllers/calendarioController');
 router
     .route('/calendario')
     .post((req, res) => calendarioController.create(req, res))
-    .delete((req, res) => calendarioController.removerEvento(req, res))
+    .delete((req, res) => calendarioController.delete(req, res));
 
 router
     .route('/eventos')
     .post((req, res) => calendarioController.getAll(req, res))
+    
 module.exports = router;
